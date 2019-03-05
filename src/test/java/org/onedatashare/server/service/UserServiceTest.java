6dd53a6bca0eecbe6d7049dca96d3186f6295143
RyChrome <dilsy3@gmail.com>
2019-03-05 11:18:43 -0500

    Added tests related to creating user

--- src/test/java/org/onedatashare/server/service/UserServiceTest.java
+++ src/test/java/org/onedatashare/server/service/UserServiceTest.java
@@ -1,6 +1,9 @@
 package org.onedatashare.server.service;
 
+import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.onedatashare.server.model.core.User;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -11,6 +14,9 @@ public class UserServiceTest {
     @Autowired
     private UserService userService;
 
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
     @Test
     public void createUserTest() throws Exception {
 //    User user = new User("vanditsa@buffalo.edu", "asdasd");
@@ -18,8 +24,26 @@ public class UserServiceTest {
     }
 
     @Test
-    public void createUser_givenNothing_createsUser() {
+    public void createUser_givenBlankEmailAndPassword_throwsRuntimeExceptionAndDisplaysCorrectMessage() {
+        thrown.expect(RuntimeException.class);
+        thrown.expectMessage("No password was provided");
+        User user = new User("","");
+        userService.createUser(user).subscribe();
+    }
 
+    @Test
+    public void createUser_givenRandomEmailAndBlankPassword_throwsRuntimeExceptionAndDisplaysCorrectMessage() {
+        thrown.expect(RuntimeException.class);
+        thrown.expectMessage("No password was provided");
+        User user = new User("ryandils@buffalo.edu","");
+        userService.createUser(user).subscribe();
+    }
+
+    @Test
+    public void createUser_givenBlankEmailAndRandomPassword_throwsNullPointerException() {
+        thrown.expect(NullPointerException.class);
+        User user = new User("","password");
+        userService.createUser(user).subscribe();
     }
 
     @Test
